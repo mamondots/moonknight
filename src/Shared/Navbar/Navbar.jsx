22c521eed7b5d48248a4b058cab5c8a95321fcfc
mamondots <shikderr112@gmail.com>
2023-10-06 22:16:41 +0600

    create navbar

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,41 @@
-
+import { BiSearch,BiHeart,BiMenu } from 'react-icons/bi';
+import {AiOutlineShoppingCart } from 'react-icons/ai';
+import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <div>
-            this is nav
+        <div className='lg:px-20 md:px-12 px-12 py-6 bg-white'>
+            <div className='flex items-center justify-between'>
+                <div>
+                    <h2 className='text-xl font-semibold'>MamonDots</h2>
+                </div>
+
+                <div className='hidden lg:block'>
+                <div className='flex items-center space-x-12'>
+                    <div className=''>
+                        <ul className='flex items-center space-x-6 nav-link text-lg'>
+                            <li><a href="">Home</a></li>
+                            <li><a href="">All jewelry</a></li>
+                            <li><a href="">Blogs</a></li>
+                            <li><a href="">About us</a></li>
+                            <li><a href="">Contact us</a></li>
+                        </ul>
+                    </div>
+
+                    <div className='flex items-center space-x-4 text-xl'>
+                         <p><BiSearch></BiSearch></p>
+                         <p><BiHeart></BiHeart></p>
+                         <p><AiOutlineShoppingCart></AiOutlineShoppingCart></p>
+                         <p>Login</p>
+                    </div>
+                </div>
+                </div>
+
+                <div className='lg:hidden block'>
+                     <p className='text-2xl px-2 py-2 rounded-md text-white bg-[#E85B40] hover:scale-105 duration-300 cursor-pointer'><BiMenu></BiMenu></p>
+                </div>
+
+            </div>
         </div>
     );
 };
